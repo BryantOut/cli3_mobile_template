e0dcfc0e08d8d6d8cf795a2ef02de43a3eb3cce0
DESKTOP-F52EJ6F\Administrator <bryantout@163.com>
2021-02-20 17:33:31 +0800

    new: 根据说明新建项目，并引入vw适配方案

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+  .box {
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+  }
+</style>
